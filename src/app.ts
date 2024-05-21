243f957fd4533fe7f03d422b41dd55ca09019bc5
Jafir Anjum <jafiranjum2020@gmail.com>
2024-05-21 11:45:05 +0600

    product interface,model and basic router setup done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from 'express';
+import { ProductRouters } from './modules/product/product.route';
 const app = express();
+
+
+app.use('/api/products', ProductRouters);
 
 app.get('/', (req:Request, res: Response) => {
   res.send('Hello World!')
