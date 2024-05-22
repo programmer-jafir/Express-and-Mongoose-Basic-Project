4983d6fd895c47e266617936fdccd3cf1cbc46d7
Jafir Anjum <jafiranjum2020@gmail.com>
2024-05-22 23:20:01 +0600

    Route not found added

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { ProductRouters } from './modules/product/product.route';
 import { OrderRouters } from './modules/order/order.router';
 const app = express();
 
 //parser
 app.use(express.json())
-
 app.use('/api/products', ProductRouters);
 app.use('/api/orders', OrderRouters);
 
@@ -13,4 +12,8 @@ app.get('/', (req:Request, res: Response) => {
   res.send('Hello World!')
 })
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const error = new Error('Route not found');
+  res.status(404).json({ success: false, message: error.message });
+});
 export default app
